b642272073c3aa286561b1ac17bb27283d6ca7e8
Andreas Pfohl <mail@apfohl.com>
2019-10-06 16:28:28 +0200

    Fixed stale pid file for Rsyslog

--- boot.sh
+++ boot.sh
@@ -5,6 +5,11 @@ TIMEZONE="${TZ:-UTC}"
 cp "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
 echo $TIMEZONE > /etc/timezone
 
+# Rsyslog
+if [ -f "/var/run/rsyslogd.pid" ]; then
+    rm -f /var/run/rsyslogd.pid
+fi
+
 # Boot sequence
 for file in /boot.d/*; do
     if [ -x "$file" ]; then
